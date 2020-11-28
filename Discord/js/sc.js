25277f37847f494315bc8ffe73483d165b9f7fa9
Yunus Can Güngördü <72223095+YunusCanGungordu@users.noreply.github.com>
2020-11-28 20:28:52 +0300

    Update sc.js
    hide/show js has fixed.

--- Discord/js/sc.js
+++ Discord/js/sc.js
@@ -1,36 +1,14 @@
-		function görünebilir(){
-			var ID;
-			ID = document.getElementById('detaylar');
+function görünebilir(){
+    var ID;
+    ID = document.getElementById("detaylar");
 
-			if (ID.style.visibility="hidden") {
-				ID.style.visibility="visible";
-			} else {
-				ID.style.visibility="hidden";
-			}
-        }
+    if (ID.style.visibility == "hidden") {
+            ID.style.visibility = "visible";
+            console.log("Kullanıcı destek bilgerini görüntüledi.");
+        } else {
+            ID.style.visibility = "hidden";
+            console.log("Kullanıcı destek bilgerini kapattı.");
+    }
+}
 
-
-        var dil;
-        dil = "python"; 
-        if (dil == "nodejs") {
-            document.write("Yapay zeka hazırlanırken NodeJS kullanılmıştır.")
-        } else if (dil == "java") {
-            document.write("Yapay zeka hazırlanırken Java dili kullanılmıştır.")
-        } else if (dil == "python") {
-            document.write("Yapay zeka hazırlanırken Python dili kullanılmıştır.")
-        }
-        
-        var dil;
-        dil = "python"; 
-
-        if (dil == "java") {
-            document.write('<button style="background-color: transparent; border: 0px; font-size: 20px;"><i class="fab fa-java" style="color: white;"></i></button>');
-        } else if (dil == "python") {
-            document.write('<button style="background-color: transparent; border: 0px; font-size: 20px;"><i class="fab fa-python" style="color: white;"></i></button>');
-        } else if (dil == "nodejs"){
-            document.write('<button style="background-color: transparent; border: 0px; font-size: 20px;"><i class="fab fa-node-js" style="color: white;"></i></button>');
-        }    
-        
-        
-// Bu kodlar siteye entegre değidlir, zaten onlar html sayfalarında derlendi. 
-// Buradakiler sadece arşiv. Yani siteye etkisi yok.        
+console.log("Sayfa yüklendi.");
